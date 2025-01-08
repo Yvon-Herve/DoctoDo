c7eea6060623ba7df6a06e89ad0525a84ef46f22
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-08 15:16:25 +0100

    architecture plus asset

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contact from "./pages/Contact";
 import MyAppointments from "./pages/MyAppointments";
 import MyProfil from "./pages/MyProfil";
 import About from "./pages/About";
+import Appointment from "./pages/Appointment";
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/myappointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
         <Route path="/myprofil" element={<MyProfil />} />
       </Routes>
     </div>
---
+++ src/pages/Appointment.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import Navigation from "../components/Navigation";
+
+const Appointment = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Appointenement</h1>
+    </div>
+  );
+};
+
+export default Appointment;
