4d00ff9a789d5de73fa3474e00086e07b45eb88f
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-08 14:48:51 +0100

    architecture plus asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import {} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
+import "./index.css";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>j</h1>
-      </div>
-    </>
+    <div>
+      <h1 className=" text-green-500">Hello world!</h1>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/MyProfil.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+const MyProfil = () => {
+  return <div></div>;
+};
+
+export default MyProfil;
