a55b78f4dba60fe22e39d5a9dc54eaf9173a3fe8
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-16 15:09:11 +0100

    page medecins

--- src/components/RelatedDoctors.jsx
+++ src/components/RelatedDoctors.jsx
@@ -26,7 +26,10 @@ const RelatedDoctors = ({ speciality, docId }) => {
         <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
           {relDoc.slice(0, 5).map((item, index) => (
             <div
-              onClick={() => navigate(`/appointment/${item._id}`)}
+              onClick={() => {
+                navigate(`/appointment/${item._id}`);
+                scrollTo(0, 0);
+              }}
               className="border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
               key={index}
             >
