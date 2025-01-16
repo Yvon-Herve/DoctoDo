7109871d8a9ab24e4320e6c602fae52e8ef13563
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-16 16:36:30 +0100

    page login

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,13 @@ const About = () => {
         </p>
       </div>
 
-      <div>
-        <img src={assets.about_image} alt="" />
-        <div>
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
           <p>
             Bienvenue chez DoctoDo, votre partenaire de confiance pour gérer vos
             besoins en soins de santé de manière pratique et efficace. Chez
@@ -29,13 +33,42 @@ const About = () => {
             gériez des soins continus, DoctoDo est là pour vous accompagner à
             chaque étape du processus.
           </p>
-          <b>Notre vision</b>
+          <b className="text-gray-800">Notre vision</b>
           <p>
             Notre vision chez DoctoDo est de créer une expérience de soins de
             santé transparente pour chaque utilisateur. Notre objectif est de
             combler le fossé entre les patients et les prestataires de soins de
             santé, en vous permettant d'accéder plus facilement aux soins dont
             vous avez besoin, quand vous en avez besoin.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl my-4">
+        <p>
+          POURQUOI{" "}
+          <span className="text-gray-700 font-semibold">NOUS CHOISIR</span>
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Efficacité</b>
+          <p>
+            Planification de rendez-vous rationalisée qui s'adapte à votre style
+            de vie chargé.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Commodité</b>
+          <p>
+            Accédez à un réseau de professionnels de la santé de confiance dans
+            votre région.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Personnalisation</b>
+          <p>
+            Recommandations et rappels personnalisés pour vous aider à rester au
+            top de votre santé.
           </p>
         </div>
       </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,39 @@
-// import React from 'react';
+import { assets } from "../assets/assets_frontend/assets";
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <div className="text-center text-2x1 pt-10 text-gray-500">
+        <p>
+          CONTACTEZ <span className="text-gray-700 font-semibold">NOUS</span>
+        </p>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.contact_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-lg text-gray-600">NOTRE BUREAU</p>
+          <p className="text-gray-500">
+            3 avenue de guyenne <br />
+            75000 Paris
+          </p>
+          <p className="text-gray-500">
+            Tel 02 23 45 67 12 <br /> Email: Herve.Munyemanzi@gmail.com{" "}
+          </p>
+          <p className="font-semibold text-lg text-gray-600">
+            Carrières chez DoctoDo
+          </p>
+          <p className="text-gray-500">
+            Apprenez-en davantage sur nos équipes et nos offres d’emploi.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explorer les emplois
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
