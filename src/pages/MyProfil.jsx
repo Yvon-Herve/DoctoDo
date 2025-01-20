a56e51e4554bb178fc82131bd5bcc10acc111ded
Munyemanzi <Herve.Munyemanzi@gmail.com>
2025-01-20 10:29:30 +0100

    page myprofil

--- src/pages/MyProfil.jsx
+++ src/pages/MyProfil.jsx
@@ -1,11 +1,14 @@
-// import React from "react";
+import { useState } from "react";
+import { assets } from "../assets/assets_frontend";
 
 const MyProfil = () => {
-  return (
-    <div>
-      <h1>Myprofil</h1>
-    </div>
-  );
+  const [userData, setUserData] = useState({
+    name: "Yvon Desire",
+    image: assets.profil_pic,
+    email: "yvondesire@gmail.com",
+    tel: "0223456789",
+  });
+  return <div></div>;
 };
 
 export default MyProfil;
